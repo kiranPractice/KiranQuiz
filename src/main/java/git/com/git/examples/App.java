15e1149af5e011dfe39fe3d750e0800084875e00
kiranPractice <bb.79451792@gmail.com>
2024-05-24 14:44:31 +0530

    git practice

---
+++ src/main/java/git/com/git/examples/App.java
@@ -0,0 +1,13 @@
+package git.com.git.examples;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/main/java/git/com/git/examples/Hi.java
@@ -0,0 +1,18 @@
+package git.com.git.examples;
+
+public class Hi {
+
+	public static void main(String[] args) {
+
+		int n = 100;
+		if (n > 20) {
+			System.out.println("Bigger");
+
+		} else if (n == 0) {
+			System.out.println("nothing");
+		} else {
+			System.out.println("smaller");
+		}
+	}
+
+}
